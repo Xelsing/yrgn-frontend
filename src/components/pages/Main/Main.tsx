77119b4f6bd7a7b041eb3f650de2d8846f069532
Ivan Yarigin <vano.03.06@gmail.com>
2023-09-05 02:17:17 +0300

    add kitty

--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Main.module.scss';
+import Image from "next/image";
 
 const Main = () => {
   return (
@@ -7,8 +8,23 @@ const Main = () => {
       <div className={styles.Inner}>
         <h1 className={styles.Title}>Main page title</h1>
         <div className={styles.Content}>
-          <div className={`${styles.box_1} ${styles.card}`}></div>
-          <div className={`${styles.box_2} ${styles.card}`}></div>
+          {/*<div className={`${styles.box_1} ${styles.card}`}></div>*/}
+          {/*<div className={`${styles.box_2} ${styles.card}`}></div>*/}
+            <Image
+              className={styles.image}
+              src={'/1.jpg'}
+              width={960}
+              height={1280}
+              // fill={true}
+              alt={''}
+            />
+            <Image
+              className={styles.image}
+              src={'/2.jpg'}
+              width={960}
+              height={1280}
+              alt={''}
+            />
           {/*<div className={`${styles.box_3} ${styles.card}`}></div>*/}
           {/*<div className={`${styles.box_4} ${styles.card}`}></div>*/}
         </div>
